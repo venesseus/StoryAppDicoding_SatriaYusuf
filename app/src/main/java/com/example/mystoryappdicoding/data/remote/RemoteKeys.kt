b1efc3297817050b36f7d669fad4ae7b2232182a
satri <satriayusufrafaly@gmail.com>
2023-05-08 18:17:22 +0700

    Last Update

--- app/src/main/java/com/example/mystoryappdicoding/data/remote/RemoteKeys.kt
+++ app/src/main/java/com/example/mystoryappdicoding/data/remote/RemoteKeys.kt
@@ -3,6 +3,7 @@ package com.example.mystoryappdicoding.data.remote
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+//Prevkey
 @Entity(tableName = "remote_keys")
 data class RemoteKeys(
     @PrimaryKey
